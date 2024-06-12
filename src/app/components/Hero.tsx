fb33c420f16bf798899933f9178ca8a3f638bea8
rossduris <rossduris@gmail.com>
2024-06-12 15:12:03 -0400

    updating home animations

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -3,11 +3,11 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="   flex flex-col justify-around items-center  lg:my-20 my-10 xl:mt-32  ">
-      <h1 className=" text-6xl sm:text-7xl text-white font-bold mt-32 p-20 text-shadow">
+    <div className="   flex flex-col justify-around items-center  lg:my-20 my-10 xl:mt-32">
+      <h1 className=" text-6xl sm:text-7xl text-white font-bold sm:mt-32 my-10 pt-10 text-shadow">
         Culinary Mastery
       </h1>
-      <span className=" text-shadow bookmark-font text-white my-5">With </span>
+      <span className=" text-shadow bookmark-font text-white my-2">With </span>
       <>
         <span className=" text-white text-[10em] text-shadow  signature-font leading-[120px] text-center my-10 ">
           Raphael Savoureux
